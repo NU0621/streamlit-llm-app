7fb659ae93226009b09e01a33d97ed61c4113040
あなたのGitHubユーザー名 <dolcelife1@gmail.com>
2025-03-13 12:06:28 +0900

    Fix imports for latest LangChain structure

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI  # 修正
+from langchain_core.messages import HumanMessage, SystemMessage  # 修正
 from dotenv import load_dotenv
 import os
 
